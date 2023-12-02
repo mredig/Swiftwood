936f62e80196c89217eb342f416c27bf4ca80755
Michael Redig <mredig@gmail.com>
2023-12-02 01:47:24 -0600

    (refactor) send data to STDOUT instead of `print()`

--- Sources/Swiftwood/ConsoleLogDestination.swift
+++ Sources/Swiftwood/ConsoleLogDestination.swift
@@ -6,9 +6,16 @@ public class ConsoleLogDestination: SwiftwoodDestination {
 	public var logFilter: LogCategory.Filter = .none
 	public var shouldCensor: Bool
 
-	public var flushImmediately = false
+	@available(macOS 10.15, iOS 13.0, tvOS 13.0, *)
+	public var flushImmediately: Bool {
+		get { _flushImmediately }
+		set { _flushImmediately = newValue }
+	}
+	private var _flushImmediately = false
 
 	public var maxBytesDisplayed: Int
+
+	private let stdOut = FileHandle.standardOutput
 
 	public init(maxBytesDisplayed: Int, shouldCensor: Bool = false) {
 		self.maxBytesDisplayed = maxBytesDisplayed
@@ -24,7 +31,10 @@ public class ConsoleLogDestination: SwiftwoodDestination {
 		}
 
 		guard
-			let sectionEndIndex = formattedMessage.index(formattedMessage.startIndex, offsetBy: maxBytesDisplayed, limitedBy: formattedMessage.endIndex)
+			let sectionEndIndex = formattedMessage.index(
+				formattedMessage.startIndex,
+				offsetBy: maxBytesDisplayed,
+				limitedBy: formattedMessage.endIndex)
 		else {
 			print(formattedMessage)
 			return
@@ -33,7 +43,29 @@ public class ConsoleLogDestination: SwiftwoodDestination {
 		let firstXCharacters = formattedMessage[formattedMessage.startIndex..<sectionEndIndex]
 
 		print(firstXCharacters + "... (too large to output in console)")
-		guard flushImmediately else { return }
-		fflush(stdout)
+	}
+
+	private func print(_ args: Any ..., terminator: String = "\n") {
+		let stringified = args
+			.map { String(describing: $0) }
+			.joined(separator: " ")
+			.appending(terminator)
+			.utf8
+		let data = Data(stringified)
+		do {
+			if #available(macOS 10.15.4, iOS 13.4, tvOS 13.4, *) {
+				try stdOut.write(contentsOf: data)
+			} else {
+				stdOut.write(data)
+			}
+			if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+				if flushImmediately {
+					try stdOut.synchronize()
+				}
+			}
+		} catch {
+			Swift.print("Error sending to stdout: \(error)")
+			Swift.print(stringified)
+		}
 	}
 }
